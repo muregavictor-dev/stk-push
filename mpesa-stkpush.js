fa203ad33197f0b60d433becc5d387c6224a9c2b
victor murithi murega <muregavictor24@gmail.com>
2025-05-15 16:39:06 +0300

    Update mpesa-stkpush.js

--- mpesa-stkpush.js
+++ mpesa-stkpush.js
@@ -4,10 +4,10 @@ export default async function handler(req, res) {
   if (req.method !== 'POST') return res.status(405).end('Method Not Allowed');
 
   const { phone } = req.body;
-  const consumerKey = process.env.DARAJA_CONSUMER_KEY;
-  const consumerSecret = process.env.DARAJA_CONSUMER_SECRET;
-  const shortcode = process.env.DARAJA_SHORTCODE;
-  const passkey = process.env.DARAJA_PASSKEY;
+  const consumerKey = tAnaAxV9y7AxfPFOifAYZyf7Vmvx6xeSHkgoF0fbSGYvJ6FK;
+  const consumerSecret = NCwEuRjDPYu484egtsXk3AzVTYpkM29z1dvMOogJxoOs6tZNGaEQV6RVXdfRrAHP;
+  const shortcode = 174379;
+  const passkey = ME1y5rjZwqrWiY6E6BaI8mzuCaGM95jPdOH04hR5FchfXk9KanoU27c85mnmN+pUtug/yJI5VVGAb0Tlmvj/ZDe6vlq33njQ5fFXFHKctfzumsNx524C0yoH1NNPkATQSbJ2RjDN5TGF2EytFiTkgSp5+rvYvV4b8h6jOq+QBWjGyq4+glw2OdtKerJR09i/DmkEefbB7WG7Km8+HLYKCicZRrNAoK8cxCLenb2obCc3Ngbs94OtelUHXvYeHejk7eiVqS/yF/Uz7cbWf3ZMXlb3R1TAQmSRB8JyiuPmMbc/dk7kBIWXKnUIPTXCY9kx1vmBm1jPaLplbHnVuOG84Q==;
 
   const auth = Buffer.from(`${consumerKey}:${consumerSecret}`).toString('base64');
 
